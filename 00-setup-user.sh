df7af37b3174d2ab15418a1246a71bfc788bfb5d
Daniela Scarpa <daniscarpa8593@gmail.com>
2026-03-05 16:35:09 +0100

    Update 00-setup-user.sh

--- 00-setup-user.sh
+++ 00-setup-user.sh
@@ -10,8 +10,31 @@ if [ -z "$1" ]; then
 fi
 
 NEW_USER=$1
-SSH_FOLDER=/home/$NEW_USER/.ssh
 
+# Create the new user
+# -m: create the home folder /home/$NEW_USER
+# -G wheel: add the user to sudoers
+# -s: set the default shell
+useradd -m -G wheel -s /bin/bash $NEW_USER
+
+# Takes the password as an input.
+echo "Setting Password for $NEW_USER.."
+passwd $NEW_USER
+
+# Duplicated the ssh key from root
+SSH_FOLDER=/home/$NEW_USER/.ssh
 mkdir -p $SSH_FOLDER
 cp /root/.ssh/* $SSH_FOLDER
+# Change the owner of .ssh and subfolders to user
 chown -R "$NEW_USER:$NEW_USER" $SSH_FOLDER
+# Change access of .ssh folder to only user can access
+chmod 700 $SSH_FOLDER
+# Change access of ssh keys to only user can read and write
+chmod 600 $SSH_FOLDER/*
+
+WORKSPACE=/home/$NEW_USER/workspace
+mkdir -p $WORKSPACE
+mv /root/arch-linux $WORKSPACE
+
+su $NEW_USER -c "cd $WORKSPACE && bash"
+
